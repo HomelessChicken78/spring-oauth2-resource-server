cf8b571063a92f429452145650d158781037f780
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-07-06 15:29:22 +0200

    FEAT: add methods in repository to find a profile based on the user

--- src/main/java/it/itsacademy/springoauth2resourceserver/repository/UserProfileRepository.java
+++ src/main/java/it/itsacademy/springoauth2resourceserver/repository/UserProfileRepository.java
@@ -1,11 +1,22 @@
 package it.itsacademy.springoauth2resourceserver.repository;
 
+import it.itsacademy.springoauth2resourceserver.exception.NotFoundException;
 import it.itsacademy.springoauth2resourceserver.model.User;
 import it.itsacademy.springoauth2resourceserver.model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> {
     boolean existsByUser(User user);
+
+    Optional<UserProfile> findFirstByUser(User user);
+
+    default UserProfile findFirstByUserOrElseThrow(User user) {
+        return findFirstByUser(user)
+                .orElseThrow(
+                        () -> new NotFoundException("The user " + user.getUserId() + " doesn't have any profile")
+                );
+    }
 }
