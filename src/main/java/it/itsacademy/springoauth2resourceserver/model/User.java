bb0bc4b37f4e415c39281969b6767ed376e00ec6
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-07-06 10:58:20 +0200

    FEAT: add default values for attributes "roles" and "isActive"

--- src/main/java/it/itsacademy/springoauth2resourceserver/model/User.java
+++ src/main/java/it/itsacademy/springoauth2resourceserver/model/User.java
@@ -15,8 +15,8 @@ public class User {
     @Column(nullable = false, unique = true) private String sub;
     @Column(nullable = false, unique = true) private String email;
     // NB: se si usano altri issuer andrebbe aggiunta una colonna "issuer" e sub (ed email) non può esser unique
-    @NotEmpty @ElementCollection @Enumerated(EnumType.STRING) private Set<Role> roles;
-    @Column(nullable = false) private boolean isActive;
+    @NotEmpty @ElementCollection @Enumerated(EnumType.STRING) private Set<Role> roles = Set.of(Role.USER);
+    @Column(nullable = false) private boolean isActive = true;
 
     public enum Role {
         ADMIN, MANAGER, USER
