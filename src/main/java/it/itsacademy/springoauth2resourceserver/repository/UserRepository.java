5b47b7049e03b8bd35bed0a725dec527ce673b5b
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-07-06 15:13:10 +0200

    FEAT: add new method in repository to search a user by sub or throw an exception if not found

--- src/main/java/it/itsacademy/springoauth2resourceserver/repository/UserRepository.java
+++ src/main/java/it/itsacademy/springoauth2resourceserver/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package it.itsacademy.springoauth2resourceserver.repository;
 
+import it.itsacademy.springoauth2resourceserver.exception.NotFoundException;
 import it.itsacademy.springoauth2resourceserver.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,5 +8,12 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    public Optional<User> findBySub(String sub);
+    Optional<User> findBySub(String sub);
+
+    default User findBySubOrElseThrow(String sub) {
+        return findBySub(sub)
+                .orElseThrow(
+                        () -> new NotFoundException("Could not find any user with sub " + sub)
+                );
+    }
 }
