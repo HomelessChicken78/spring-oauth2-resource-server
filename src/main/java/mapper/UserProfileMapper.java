4237d619c8a0ebbd9fb1ce97fd76e063e8dc753f
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-07-06 15:30:45 +0200

    REFACTOR: removed "public" statement for method "toDto"

--- src/main/java/mapper/UserProfileMapper.java
+++ src/main/java/mapper/UserProfileMapper.java
@@ -6,5 +6,5 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserProfileMapper {
-    public UserProfileResponseDTO toDto(UserProfile entity);
+    UserProfileResponseDTO toDto(UserProfile entity);
 }
