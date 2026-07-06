bbc6f81fe59bb3b0cfe877166b66d69f6308dae8
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-07-06 09:43:45 +0200

    REFACTOR: update validation to check for url

--- src/main/java/it/itsacademy/springoauth2resourceserver/dto/UserProfileRegistrationDTO.java
+++ src/main/java/it/itsacademy/springoauth2resourceserver/dto/UserProfileRegistrationDTO.java
@@ -1,11 +1,14 @@
 package it.itsacademy.springoauth2resourceserver.dto;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @AllArgsConstructor @NoArgsConstructor
 @Data
 public class UserProfileRegistrationDTO {
-    @NotEmpty
+    @Pattern(
+            regexp = "https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)",
+            message = "Invalid URL"
+    )
     private String avatarUrl;
 }
