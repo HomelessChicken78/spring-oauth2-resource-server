54462f4549a693f4b51c1c0a0ea823c374b95cda
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-07-07 12:14:54 +0200

    STYLE: remove no longer needed comment

--- src/main/java/it/itsacademy/springoauth2resourceserver/controller/AuthController.java
+++ src/main/java/it/itsacademy/springoauth2resourceserver/controller/AuthController.java
@@ -16,8 +16,6 @@ public class AuthController {
     @PutMapping(path = "/signup", consumes = json)
     @ResponseStatus(HttpStatus.CREATED)
     public void signup(@RequestBody UserProfileRegistrationDTO newUser) {
-        // NB: non è la soluzione "corretta" passare l'id token al resource server. Se proprio serve il contenuto si può
-        // usare OICD (eg con GET su /oauth2/userInfo)
         authService.signup(newUser);
     }
 
