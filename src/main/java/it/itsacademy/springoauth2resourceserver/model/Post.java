426984ba367e6a57159fa7d0a3a0b6c12eeff98c
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-07-10 11:30:26 +0200

    FEAT: create new entity

---
+++ src/main/java/it/itsacademy/springoauth2resourceserver/model/Post.java
@@ -0,0 +1,21 @@
+package it.itsacademy.springoauth2resourceserver.model;
+
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Document
+@AllArgsConstructor @NoArgsConstructor @Builder
+@Getter @Setter @ToString @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Post {
+    @Id @EqualsAndHashCode.Include private ObjectId id;
+    private String title, content, topic, author;
+
+    @Size(max = 7, message = "There can't be more than 7 related posts")
+    @ToString.Exclude private Set<ObjectId> relatedPosts = new HashSet<>();
+}
