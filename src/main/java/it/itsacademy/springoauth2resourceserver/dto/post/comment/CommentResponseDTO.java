20d0eae05b35c7caf0ec3248a0dcd49002237ef6
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-07-14 16:44:23 +0200

    FEAT: add CommentResponseDTO for comment response handling

---
+++ src/main/java/it/itsacademy/springoauth2resourceserver/dto/post/comment/CommentResponseDTO.java
@@ -0,0 +1,12 @@
+package it.itsacademy.springoauth2resourceserver.dto.post.comment;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor @NoArgsConstructor
+@Data
+public class CommentResponseDTO {
+    private String id, content;
+    private LocalDateTime createdAt, updatedAt;
+}
