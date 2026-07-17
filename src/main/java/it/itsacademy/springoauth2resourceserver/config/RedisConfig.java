d237f5a3b82758a3c513fcf447aaf7b2ffa3181c
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-07-17 17:14:52 +0200

    CHORE: hopefully fixed redis config

--- src/main/java/it/itsacademy/springoauth2resourceserver/config/RedisConfig.java
+++ src/main/java/it/itsacademy/springoauth2resourceserver/config/RedisConfig.java
@@ -1,13 +1,13 @@
 package it.itsacademy.springoauth2resourceserver.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.cache.autoconfigure.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import tools.jackson.databind.ObjectMapper;
 
 import java.time.Duration;
 
@@ -18,13 +18,34 @@ public class RedisConfig {
     private Long ttlDuration;
 
     @Bean
-    public RedisCacheConfiguration cacheConfiguration(ObjectMapper objectMapper) {
+    public RedisCacheConfiguration cacheConfiguration() {
+        // The previous GenericJackson2JsonRedisSerializer is deprecated and can't be used. GenericJackson2JsonRedisSerializer
+        // used to take no argument and created their own object mapper, which automatically asked for the class where the
+        // cache originated from. Now that it is deprecated, we have to use GenericJacksonJsonRedisSerializer, which requires
+        // an object mapper. However, we can't inject the one from spring, since it does not support classes. In fact,
+        // if we use it, redis will try to ask for the class without succeeding, thus defaulting to trying to convert
+        // to a LinkedHashMap. It may technically work if every method returns a LinkedHashMap or a subclass, however
+        // if you do something like (LinkedHashMap) Animal it will crash.
+        // Why does the Spring ObjectMapper often fail? Because it is configured for Rest API, where you don't want to return
+        // the class for security reasons (obviously).
+        // Solution: use the GenericJacksonJsonRedisSerializer builder, which creates a Redis-specific serializer
+        // configuration with the required type information so cached objects can be restored to their original classes.
+        GenericJacksonJsonRedisSerializer serializer = GenericJacksonJsonRedisSerializer.builder().build();
+
         return RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofSeconds(ttlDuration))
                 .disableCachingNullValues()
                 .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair
-                                .fromSerializer(new GenericJacksonJsonRedisSerializer(objectMapper))
+                        RedisSerializationContext.SerializationPair.fromSerializer(serializer)
                 );
+    }
+
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+        return (builder) -> builder
+                .withCacheConfiguration("itemCache",
+                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(10)))
+                .withCacheConfiguration("customerCache",
+                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5)));
     }
 }
