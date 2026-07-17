42d39fbe3306ff48a41fc00331897b574fa33f4f
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-07-17 11:47:33 +0200

    CHORE: improve redis configuration with time to leave

--- src/main/java/it/itsacademy/springoauth2resourceserver/config/RedisConfig.java
+++ src/main/java/it/itsacademy/springoauth2resourceserver/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package it.itsacademy.springoauth2resourceserver.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -7,12 +8,18 @@ import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializ
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import tools.jackson.databind.ObjectMapper;
 
+import java.time.Duration;
+
 @Configuration
 public class RedisConfig {
+    @Value("${REDIS_TTL_DURATION_SECONDS:3600}")
+    private Long ttlDuration;
 
     @Bean
     public RedisCacheConfiguration cacheConfiguration(ObjectMapper objectMapper) {
         return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(ttlDuration))
+                .disableCachingNullValues()
                 .serializeValuesWith(
                         RedisSerializationContext.SerializationPair
                                 .fromSerializer(new GenericJacksonJsonRedisSerializer(objectMapper))
