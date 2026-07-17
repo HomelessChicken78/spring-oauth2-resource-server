aace2caccc064e3af8542be54220d2cc42f721b1
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-07-17 10:43:44 +0200

    CHORE: configure redis cache

---
+++ src/main/java/it/itsacademy/springoauth2resourceserver/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package it.itsacademy.springoauth2resourceserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import tools.jackson.databind.ObjectMapper;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration(ObjectMapper objectMapper) {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair
+                                .fromSerializer(new GenericJacksonJsonRedisSerializer(objectMapper))
+                );
+    }
+}
