dd85ef4457d8e23930634947c9054a8c225858fb
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-07-21 17:30:19 +0200

    FEAT: add Follow entity to manage user follow relationships

---
+++ src/main/java/it/itsacademy/springoauth2resourceserver/model/Follow.java
@@ -0,0 +1,37 @@
+package it.itsacademy.springoauth2resourceserver.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.envers.Audited;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.hibernate.envers.RelationTargetAuditMode.NOT_AUDITED;
+
+@Entity
+@Table(
+        uniqueConstraints = @UniqueConstraint(
+                columnNames = {"follower_id", "following_id"}
+        ),
+        check = @CheckConstraint(
+                name = "chk_follower_diff_following",
+                constraint = "follower_id <> following_id"
+        )
+)
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @ToString @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Audited(targetAuditMode = NOT_AUDITED)
+public class Follow {
+    @Id private UUID followId;
+
+    @ManyToOne @JoinColumn(name = "follower_id")
+    private UserProfile follower;
+
+    @ManyToOne @JoinColumn(name = "following_id")
+    private UserProfile following;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate private LocalDateTime createdAt;
+}
