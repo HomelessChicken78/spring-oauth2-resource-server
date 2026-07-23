c496d152b23497dfb53bb9a30537cfc3047a4f90
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-07-23 10:29:58 +0200

    REFACTOR: change followUser endpoint from PUT to POST for better RESTful design

--- src/main/java/it/itsacademy/springoauth2resourceserver/controller/AuthController.java
+++ src/main/java/it/itsacademy/springoauth2resourceserver/controller/AuthController.java
@@ -60,7 +60,7 @@ public class AuthController {
     }
 
     @PreAuthorize("hasRole('USER')")
-    @PutMapping(path = "/users/me/follow/{followingNickname}")
+    @PostMapping(path = "/users/me/follow/{followingNickname}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void followUser(@PathVariable String followingNickname) {
         authService.follow(followingNickname);
