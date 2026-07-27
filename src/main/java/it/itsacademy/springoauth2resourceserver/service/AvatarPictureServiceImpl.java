ab70845a14214ab42dcb8c72ea28c88a89971f43
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-07-27 12:52:40 +0200

    FEAT: implement S3 upload functionality for avatar images with URL retrieval

--- src/main/java/it/itsacademy/springoauth2resourceserver/service/AvatarPictureServiceImpl.java
+++ src/main/java/it/itsacademy/springoauth2resourceserver/service/AvatarPictureServiceImpl.java
@@ -1,13 +1,18 @@
 package it.itsacademy.springoauth2resourceserver.service;
 
-import it.itsacademy.springoauth2resourceserver.exception.BadRequestException;
-import it.itsacademy.springoauth2resourceserver.exception.ContentTooLargeException;
+import it.itsacademy.springoauth2resourceserver.exception.*;
+import it.itsacademy.springoauth2resourceserver.security.CurrentUserProvider;
 import lombok.RequiredArgsConstructor;
 import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import static org.springframework.util.StringUtils.getFilenameExtension;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.async.AsyncRequestBody;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Utilities;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,7 +25,7 @@ public class AvatarPictureServiceImpl implements AvatarPictureService {
     private String bucketS3;
 
     @Value("${S3_ROOT_PREFIX}")
-    private String rootPrefix;
+    private String rootPrefix; // Ends with "/"
 
     @Value("#{'${AVATAR_IMAGE_FORMATS:image/jpeg}'.split(',')}")
     private List<String> avatarImageFormats;
@@ -29,6 +34,9 @@ public class AvatarPictureServiceImpl implements AvatarPictureService {
     private long maxImageSize;
 
     private final Tika tika = new Tika();
+    private final S3AsyncClient s3Client;
+    private final S3Utilities s3Utilities;
+    private final CurrentUserProvider currentUser;
 
     private boolean isValidFileMimeType(MultipartFile file, List<String> allowedFormats) {
         if (file == null || file.isEmpty()) return false;
@@ -66,6 +74,18 @@ public class AvatarPictureServiceImpl implements AvatarPictureService {
         if (!isFileSizeValid(image, maxImageSize))
             throw new ContentTooLargeException("File size must be less than " + humanReadableBytes(maxImageSize) + ".");
 
+        try {
+            byte[] fileBytes = image.getBytes();
+            final String extension = tika.detect(fileBytes);
+            final String s3ObjectKey = rootPrefix + currentUser.getProfile().getNickname() + "/" + image.getOriginalFilename() + getFilenameExtension(extension);
 
+            s3Client.putObject(b -> b.bucket(bucketS3).key(s3ObjectKey).contentType(extension).build(),
+                    AsyncRequestBody.fromBytes(fileBytes)).join();
+
+            final String imageUrl = s3Utilities.getUrl(GetUrlRequest.builder().bucket(bucketS3).key(s3ObjectKey).build()).toString();
+            currentUser.getProfile().setAvatarUrl(imageUrl);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
