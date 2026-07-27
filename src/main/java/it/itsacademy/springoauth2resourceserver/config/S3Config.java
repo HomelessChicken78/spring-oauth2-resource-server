91828ebcff46598cdbc6b512928050085db24460
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-07-27 12:18:51 +0200

    CHORE: add AWS_REGION configuration for S3 client setup

--- src/main/java/it/itsacademy/springoauth2resourceserver/config/S3Config.java
+++ src/main/java/it/itsacademy/springoauth2resourceserver/config/S3Config.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 
 import java.time.Duration;
@@ -17,9 +18,13 @@ public class S3Config {
     @Value("${AWS_SECRET_ACCESS_KEY}")
     private String awsAccessKeySecret;
 
+    @Value("${AWS_REGION}")
+    private String awsRegion;
+
     @Bean
     public S3AsyncClient s3AsyncClient() {
         return S3AsyncClient.builder()
+                .region(Region.of(awsRegion))
                 .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(awsAccessKeyId, awsAccessKeySecret)))
                 .overrideConfiguration(b -> b.apiCallTimeout(Duration.ofMinutes(2)))
                 .build();
